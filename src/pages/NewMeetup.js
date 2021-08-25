83095776b06adc9b50c9ae9e30847718ee3e0a09
SahilGoliwar <55258219+SahilGoliwar@users.noreply.github.com>
2021-08-25 11:43:54 +0530

    react project

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,28 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupFrom from "../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-project-84b64-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupFrom onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+export default NewMeetupPage;
